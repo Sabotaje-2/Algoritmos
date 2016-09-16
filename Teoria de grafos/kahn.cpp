a6e61020988a50f6658f7a2c8f4fa82a492af7dc
Ivan Sandoval <nola2804@hotmail.com>
2016-09-15 19:40:15 -0500

    Subiendo kahn

---
+++ Teoria de grafos/kahn.cpp
@@ -0,0 +1,38 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+#define max_nodos 100
+// Topological sort
+int numero_nodos , numero_vertices , grado_entrada[max_nodos];
+vector < vector < int > > grafo; // Grafo con lista de adyacencia
+
+int main()
+{
+  int origen , llegada , voy;
+  while( scanf("%d %d", &numero_nodos , &numero_vertices ) , numero_nodos || numero_vertices )
+  {
+    queue < int > kahn;
+    grafo.assign( numero_nodos , vector < int >() );
+    memset( grado_entrada , 0 , sizeof grado_entrada );
+    for( int i = 0 ; i < numero_vertices ; ++i )
+    {
+      scanf("%d %d", &origen , &llegada );
+      ++grado_entrada[llegada];
+      grafo[origen].push_back( llegada );
+    }
+    for( int i = 0 ; i < numero_nodos ; ++i )
+      if( grado_entrada[i] == 0 )
+        kahn.push( i );
+    while( !kahn.empty() )
+    {
+      voy = kahn.front();
+      printf("%d\n", voy );
+      kahn.pop();
+      for( int i = 0 ; i < grafo[voy].size() ; ++i )
+        if( --grado_entrada[grafo[voy][i]] == 0 )
+          kahn.push( grafo[voy][i] );
+    }
+  }
+  return 0;
+}
