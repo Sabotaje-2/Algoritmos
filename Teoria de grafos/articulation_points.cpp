34d831bf421db6213c1252781c28e2ffd8250d03
Ivan Sandoval <nola2804@hotmail.com>
2016-09-22 21:36:08 -0500

    modificado algoritmo de tarjan

--- Teoria de grafos/articulation_points.cpp
+++ Teoria de grafos/articulation_points.cpp
@@ -12,8 +12,8 @@ int iteration_counter; // contador para ver en que recursion voy
 int root_dfs; // nodo desde el cual se comenzo el dfs
 int children; // guardar numero de hijos de la raiz
 vector < vector < int > > grafo; // grafo almacenado como lista de adyacencia
-bitset < MAX_NUMERO_NODOS > ap; // array para guardar puntos de articulacion
-
+bitset < MAX_NUMERO_NODOS > ap; // arreglo de booleanos para guardar puntos de articulacion
+// Algoritmo de tarjan
 void articulation_points( int from )
 {
   visited[from] = true;
@@ -21,6 +21,7 @@ void articulation_points( int from )
   for( int i = 0 ; i < grafo[from].size() ; ++i )
   {
     int to = grafo[from][i];
+    if( parent[from] == to )  continue;
     if( !visited[to] )
     {
       parent[to] = from;
@@ -30,7 +31,7 @@ void articulation_points( int from )
       if( dfs_low[to] >= dfs_num[from] && parent[from] != -1 )
         ap[from] = true;
     }
-    else if( parent[from] != to )
+    else
       dfs_low[from] = min( dfs_low[from] , dfs_num[to] );
   }
 }
