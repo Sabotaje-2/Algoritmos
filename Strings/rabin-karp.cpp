373ef98a54b2b546c33d297e658726a5b758cd6e
Ivan Sandoval <nola2804@hotmail.com>
2016-10-01 21:46:17 -0500

    asdsa

--- Strings/rabin-karp.cpp
+++ Strings/rabin-karp.cpp
@@ -7,9 +7,6 @@ typedef unsigned long long ull;
 
 const ull PRIME = 3ULL;
 const ull MOD = 1e9+7ULL;
-bool existe;
-char pattern[MAX_CADENA] , original_text[MAX_CADENA];
-int tam_pattern , tam_text;
 ull modular_inverse;
 
 ull fastPow( ull base , ull exponente ) // Calcular x^y en log(y)
@@ -26,20 +23,20 @@ ull fastPow( ull base , ull exponente ) // Calcular x^y en log(y)
   return llevo;
 }
 
-bool compare( int p )
+bool compare( char *original_text , char * pattern , int p )
 {
+  int tam_pattern = strlen( pattern );
     for( int i = 0 ; i < tam_pattern ; ++i )
       if( pattern[i] != original_text[i+p] ) return false;
     return true;
 }
 
 // Pattern Substring search , search pattern in original_text, worst case O( tam_pattern * tam_text ) , average case O( tam_text + tam_pattern )
-int rabin_karp( void )
+int rabin_karp( char *original_text , char *pattern )
 {
   ull hash_pattern , prime_power , hash_text ;
-  hash_pattern = hash_text = existe = false;
-  tam_pattern = strlen( pattern );
-  tam_text = strlen( original_text );
+  hash_pattern = hash_text = false;
+  int tam_pattern = strlen( pattern ) , tam_text = strlen( original_text );
   prime_power = 1;
   for( int i = 0 ; i < tam_pattern ; ++i )
   {
@@ -49,29 +46,30 @@ int rabin_karp( void )
   }
   prime_power = ( prime_power * modular_inverse ) % MOD;
   if( hash_pattern == hash_text )
-    if( compare( 0 ) )  return 0;
+    if( compare( original_text , pattern , 0 ) )  return 0;
   for( int i = tam_pattern ; i < tam_text ; ++i )
   {
     hash_text = ( hash_text - original_text[ i - tam_pattern ] + MOD ) % MOD;
     hash_text = ( hash_text * modular_inverse ) % MOD;
     hash_text = ( hash_text + ( ( original_text[i] * prime_power ) % MOD ) ) % MOD;
     if( hash_text == hash_pattern )
-      if( compare( i - tam_pattern + 1 ) ) return i - tam_pattern + 1;
+      if( compare( original_text , pattern , i - tam_pattern + 1 ) ) return i - tam_pattern + 1;
   }
   return -1;
 }
 
 int main()
 {
-  int index;
+  int index_found;
   modular_inverse = fastPow( PRIME , MOD - 2 );
+  char pattern[MAX_CADENA] , original_text[MAX_CADENA];
   while( scanf("%s%s", original_text , pattern ) != EOF )
   {
-    index = rabin_karp();
-    if( index == -1 )
+    index_found = rabin_karp( original_text , pattern );
+    if( index_found == -1 )
       puts( "Pattern not found" );
     else
-      printf("Pattern found at index %d\n", index );
+      printf("Pattern found at index %d\n", index_found );
   }
   return 0;
 }
