cb628116f78919ed24272eef4ceb1d0abcfaed7c
Ivan Dario Sandoval <nola2804@hotmail.com>
2017-02-11 10:41:36 -0500

    Add files via upload

--- Strings/rabin-karp.cpp
+++ Strings/rabin-karp.cpp
@@ -7,7 +7,7 @@ typedef unsigned long long ull;
 
 const ull PRIME = 3ULL;
 const ull MOD = 1e9+7ULL;
-ull modular_inverse;
+ull minv; // modular inverse
 int plen , tlen; // pattern length & text length
 
 ull fastPow( ull base , ull exponente ) // Calcular x^y en log(y)
@@ -24,37 +24,37 @@ ull fastPow( ull base , ull exponente ) // Calcular x^y en log(y)
   return llevo;
 }
 
-bool compare( char *original_text , char * pattern , int p )
+bool compare( char *text , char * pattern , int p )
 {
     for( int i = 0 ; i < plen ; ++i )
-      if( pattern[i] != original_text[i+p] ) return false;
+      if( pattern[i] != text[i+p] ) return false;
     return true;
 }
 
-// Pattern Substring search , search pattern in original_text, worst case O( plen * tlen ) , average case O( tlen + plen )
-int rabin_karp( char *original_text , char *pattern )
+// Pattern Substring search , search pattern in text, worst case O( plen * tlen ) , average case O( tlen + plen )
+int rabin_karp( char *text , char *pattern )
 {
   ull hp , pp , ht ; // hash_pattern , prime_power & hash_text
   hp = ht = 0ULL;
   plen = strlen( pattern );
-  tlen = strlen( original_text );
+  tlen = strlen( text );
   pp = 1;
   for( int i = 0 ; i < plen ; ++i )
   {
     hp = ( hp + pattern[i] * pp ) % MOD;
-    ht = ( ht + original_text[i] * pp ) % MOD;
+    ht = ( ht + text[i] * pp ) % MOD;
     pp = ( pp * PRIME ) % MOD;
   }
-  pp = ( pp * modular_inverse ) % MOD;
+  pp = ( pp * minv ) % MOD;
   if( hp == ht )
-    if( compare( original_text , pattern , 0 ) )  return 0;
+    if( compare( text , pattern , 0 ) )  return 0;
   for( int i = plen ; i < tlen ; ++i )
   {
-    ht = ( ht - original_text[ i - plen ] + MOD ) % MOD;
-    ht = ( ht * modular_inverse ) % MOD;
-    ht = ( ht + ( ( original_text[i] * pp ) % MOD ) ) % MOD;
+    ht = ( ht - text[ i - plen ] + MOD ) % MOD;
+    ht = ( ht * minv ) % MOD;
+    ht = ( ht + ( ( text[i] * pp ) % MOD ) ) % MOD;
     if( ht == hp )
-      if( compare( original_text , pattern , i - plen + 1 ) ) return i - plen + 1;
+      if( compare( text , pattern , i - plen + 1 ) ) return i - plen + 1;
   }
   return -1;
 }
@@ -62,11 +62,11 @@ int rabin_karp( char *original_text , char *pattern )
 int main()
 {
   int index_found;
-  modular_inverse = fastPow( PRIME , MOD - 2 );
-  char pattern[MAX_CADENA] , original_text[MAX_CADENA];
-  while( scanf("%s%s", original_text , pattern ) != EOF )
+  minv = fastPow( PRIME , MOD - 2 );
+  char pattern[MAX_CADENA] , text[MAX_CADENA];
+  while( scanf("%s%s", text , pattern ) != EOF )
   {
-    index_found = rabin_karp( original_text , pattern );
+    index_found = rabin_karp( text , pattern );
     if( index_found == -1 )
       puts( "Pattern not found" );
     else
