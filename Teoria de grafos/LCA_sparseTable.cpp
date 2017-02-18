5167d36d43805082be530a57152d0eae1ae3d2c6
Ivan Dario Sandoval <nola2804@hotmail.com>
2017-02-18 09:18:59 -0500

    Refactor lca(sparset table)

--- Teoria de grafos/LCA_sparseTable.cpp
+++ Teoria de grafos/LCA_sparseTable.cpp
@@ -4,70 +4,72 @@ using namespace std;
 
 #define MAX_N 100010
 
-#define foi( i , n , k )    for( int i = n ; i < k ; ++i )
-
 vector < vector < int > > tree;
-int parent[MAX_N];
-int height[MAX_N];
-int subTree_size[MAX_N];
-int first_ocurrence[MAX_N];
+int p[MAX_N]; // El padre inmediato de cada nodo
+int h[MAX_N]; // La latura de cada nodo
+int ssize[MAX_N]; // El tamano del subarbol, sirve para hld
+int fo[MAX_N]; // Indice de la primer ocurrencia de cada nodo en ocurrences
 vector < int > ocurrences;
 
-int dfs( int u , int alt )
+int dfs(int u , int alt = 0) // O(n)
 {
-    first_ocurrence[u] = ocurrences.size();
+    fo[u] = ocurrences.size();
     ocurrences.push_back(u);
-    height[u] = alt;
+    h[u] = alt;
     int v;
-    subTree_size[u] = 1;
-    for( int i = 0 ; i < tree[u].size() ; ++i )
+    ssize[u] = 1;
+    for(int i = 0 ; i < tree[u].size() ; ++i)
     {
         v = tree[u][i];
-        if( parent[u] == v )    continue;
-        parent[v] = u;
-        subTree_size[u] += dfs( v , alt + 1 );
+        if(p[u] == v)    continue;
+        p[v] = u;
+        ssize[u] += dfs(v , alt + 1);
         ocurrences.push_back(u);
     }
-    return subTree_size[u];
+    return ssize[u];
 }
 
-vector < vector < int > > sparse_table_lca;
+vector < vector < int > > st_lca;
 
-void build_sparse_lca( vector < int > &arr )
+void build_lca(vector < int > &arr) // O(nlog(n))
 {
-    int columns = floor( log2( arr.size() ) ) + 1;
-    sparse_table_lca.assign( arr.size() , vector < int >( columns , -2 ) );
-    for( int i = 0 ; i < arr.size() ; ++i ) sparse_table_lca[i][0] = arr[i];
-    for( int j = 1 ; j < columns ; ++j )
-        for( int i = 0 ; i < arr.size() && i + ( 1 << j ) - 1 < arr.size() ; ++i )
+    int columns = floor(log2(arr.size())) + 1;
+    st_lca.assign(arr.size() , vector < int >(columns , -2));
+    for(int i = 0 ; i < arr.size() ; ++i) st_lca[i][0] = arr[i];
+    for(int j = 1 ; j < columns ; ++j)
+        for(int i = 0 ; i < arr.size() && i + (1 << j) - 1 < arr.size() ; ++i)
         {
-            sparse_table_lca[i][j] = sparse_table_lca[i][j-1];
-            if( height[ sparse_table_lca[i + ( 1 << ( j - 1 ) )][j-1] ] < height[ sparse_table_lca[i][j] ] )
-                sparse_table_lca[i][j] = sparse_table_lca[i + ( 1 << ( j - 1 ) )][j-1];
+            st_lca[i][j] = st_lca[i][j-1];
+            if(h[ st_lca[i + (1 << (j - 1))][j-1] ] < h[ st_lca[i][j] ])
+                st_lca[i][j] = st_lca[i + (1 << (j - 1))][j-1];
         }
 }
 
-int query_sparse_lca( int i , int j )
+int lca_query(int i , int j) // O(1)
 {
-    if( i > j ) swap( i , j );
+    if(i > j) swap(i , j);
     int len = j - i + 1;
-    int k = floor( log2( len ) );
-    int ans = sparse_table_lca[i][k];
-    if( height[ sparse_table_lca[i + len - ( 1 << k )][k] ] < height[ans] )
-        ans = sparse_table_lca[i + len - ( 1 << k )][k];
+    int k = floor(log2(len));
+    int ans = st_lca[i][k];
+    if(h[st_lca[i + len - (1 << k)][k]] < h[ans])
+        ans = st_lca[i + len - (1 << k)][k];
     return ans;
 }
 
-int query_lca( int u , int v )
+int query(int u , int v)
 {
-    return query_sparse_lca( first_ocurrence[u] , first_ocurrence[v] );
+    return lca_query(fo[u] , fo[v]);
 }
 
-
-int main()
+void addEdge(int u , int v)
 {
-    dfs( 0 , 0 );
-    build_sparse_lca( ocurrences );
+    tree[u].push_back(v);
+    tree[v].push_back(u);
+}
+
+int main() // O(nlog(n) preprocess , O(1) query)
+{
+    dfs(0);
+    build_lca(ocurrences);
     return 0;
 }
-
