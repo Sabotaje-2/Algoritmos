11c00ded1ce8c55f2b4f24106447a68cc84536c7
Ivan Dario Sandoval <nola2804@hotmail.com>
2017-04-07 10:37:57 -0500

    ..

--- Estructuras de datos/sparse_table.cpp
+++ Estructuras de datos/sparse_table.cpp
@@ -5,7 +5,7 @@ struct st {
         table.assign(arr.size(), vector < int > (columns , - 2));
         foi(i, 0, table.size())  table[i][0] = arr[i];
         foi(j, 1, columns)
-            for(int i = 0 ; i + (1 << j) <= table.size() ; ++i)
+            for(int i = 0 ; i + (1 << j) <= (int)table.size() ; ++i)
               table[i][j] = max(table[i][j-1], table[i + (1 << (j - 1))][j-1]);
     }
     int query(int i, int j) {
--- Teoria de grafos/LCA_sparseTable.cpp
+++ Teoria de grafos/LCA_sparseTable.cpp
@@ -1,69 +1,44 @@
-#include <bits/stdc++.h>
-
-using namespace std;
-
-#define MAX_N 100010
-
 vector < vector < int > > tree;
-int p[MAX_N]; // El padre inmediato de cada nodo
-int h[MAX_N]; // La latura de cada nodo
-int ssize[MAX_N]; // El tamano del subarbol
-int fo[MAX_N]; // Indice de la primer ocurrencia de cada nodo en ocurrences
 vector < int > ocurrences;
+int p[MAX_N]; // El padre inmediato de cada nodo
+int h[MAX_N]; // La latura de cada nodo, MAX_N = Maximo numero de nodos
+int fo[MAX_N]; // Indice de la primer ocurrencia de cada nodo en ocurrences
 
-int dfs(int u , int alt = 0) // O(n)
-{
-    fo[u] = ocurrences.size();
-    ocurrences.push_back(u);
-    h[u] = alt;
+void dfs(int u , int alt = 0) {
     int v;
-    ssize[u] = 1;
-    for(int i = 0 ; i < tree[u].size() ; ++i)
-    {
+    fo[u] = ocurrences.size();
+    h[u] = alt;
+    ocurrences.push_back(u);
+    foi(i,0,tree[u].size()) {
         v = tree[u][i];
         if(p[u] == v)    continue;
         p[v] = u;
-        ssize[u] += dfs(v , alt + 1);
+        dfs(v, 1 + alt);
         ocurrences.push_back(u);
     }
-    return ssize[u];
 }
 
-vector < vector < int > > st_lca;
-
-void build_lca(vector < int > &arr) // O(nlog(n))
-{
-    int columns = floor(log2(arr.size())) + 1;
-    st_lca.assign(arr.size() , vector < int >(columns , -2));
-    for(int i = 0 ; i < arr.size() ; ++i) st_lca[i][0] = arr[i];
-    for(int j = 1 ; j < columns ; ++j)
-        for(int i = 0 ; i < arr.size() && i + (1 << j) - 1 < arr.size() ; ++i)
-        {
-            st_lca[i][j] = st_lca[i][j-1];
-            if(h[st_lca[i + (1 << (j - 1))][j-1]] < h[st_lca[i][j]])
-                st_lca[i][j] = st_lca[i + (1 << (j - 1))][j-1];
-        }
-}
-
-int lca_query(int i , int j) // O(1)
-{
-    if(i > j) swap(i , j);
-    int len = j - i + 1;
-    int k = floor(log2(len));
-    int ans = st_lca[i][k];
-    if(h[st_lca[i + len - (1 << k)][k]] < h[ans])
-        ans = st_lca[i + len - (1 << k)][k];
-    return ans;
-}
-
-int query(int u , int v)
-{
-    return lca_query(fo[u] , fo[v]);
-}
-
-int main() // O(nlog(n) preprocess , O(1) query)
-{
-    dfs(0);
-    build_lca(ocurrences);
-    return 0;
-}
+struct st {
+    vector < vector < int > > table;
+    st(vector < int >& arr) {
+        int columns = 1 + floor(log2(arr.size()));// C++11
+        table.assign(arr.size() , vector < int >(columns , -2));
+        foi(i,0,arr.size()) table[i][0] = arr[i];
+        for(int j = 1 ; j < columns ; ++j)
+            for(int i = 0 ; i + (1 << j) - 1 < (int)arr.size() ; ++i) {
+                table[i][j] = table[i][j-1];
+                if(h[table[i + (1 << (j - 1))][j-1]] < h[table[i][j]])
+                    table[i][j] = table[i + (1 << (j - 1))][j-1];
+            }
+    }
+    int query(int i, int j) {
+        if(i > j) swap(i, j);
+        int len = j - i + 1, k = floor(log2(len));
+        int ans = table[i][k];
+        if(h[table[i + len - (1 << k)][k]] < h[ans])
+            ans = table[i + len - (1 << k)][k];
+        return ans;
+    }
+    int lca(int u, int v) {
+        return query(fo[u], fo[v]);
+    }};
