40e7444415fc31deeb5f6fc3da918e2465dccdfd
Ivan Dario Sandoval <nola2804@hotmail.com>
2017-04-14 14:41:57 -0500

    Arreglos y renames

--- Teoria de grafos/kahn.cpp
+++ Teoria de grafos/kahn.cpp
@@ -1,38 +1,18 @@
-#include <bits/stdc++.h>
-
-using namespace std;
-
-#define max_nodos 100
-// Topological sort
-int numero_nodos , numero_aristas , grado_entrada[max_nodos];
-vector < vector < int > > grafo; // Grafo con lista de adyacencia
-
-int main()
-{
-  int origen , llegada , voy;
-  while( scanf("%d %d", &numero_nodos , &numero_aristas ) , numero_nodos || numero_aristas )
-  {
-    queue < int > kahn;
-    grafo.assign( numero_nodos , vector < int >() );
-    memset( grado_entrada , 0 , sizeof grado_entrada );
-    for( int i = 0 ; i < numero_aristas ; ++i )
-    {
-      scanf("%d %d", &origen , &llegada );
-      ++grado_entrada[llegada];
-      grafo[origen].push_back( llegada );
-    }
-    for( int i = 0 ; i < numero_nodos ; ++i )
-      if( grado_entrada[i] == 0 )
-        kahn.push( i );
-    while( !kahn.empty() )
-    {
-      voy = kahn.front();
-      printf("%d\n", voy );
-      kahn.pop();
-      for( int i = 0 ; i < grafo[voy].size() ; ++i )
-        if( --grado_entrada[grafo[voy][i]] == 0 )
-          kahn.push( grafo[voy][i] );
-    }
+int in[MAX_NODOS]; //hacer memset antes de leer
+vector < vector < int > > g;
+vector < int > kahn() {// return toposort
+  vector < int > ans;
+  queue < int > kahn;
+  int u;
+  foi(i, 0, g.size())
+    if(!in[i])  kahn.push(i);
+  while(!kahn.empty()) {
+    u = kahn.front();
+    ans.push_back(u);
+    kahn.pop();
+    foi(i, 0, g[u].size())
+      if(!--in[g[u][i]])
+        kahn.push(g[u][i]);
   }
-  return 0;
+  return ans;
 }
