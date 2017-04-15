d4bc1d71fbc824c4148899ae8845b437dcf9911f
Ivan Dario Sandoval <nola2804@hotmail.com>
2017-04-15 18:06:23 -0500

    ..

--- Teoria de grafos/Kuhn.cpp
+++ Teoria de grafos/Kuhn.cpp
@@ -1,5 +1,5 @@
 vector < vector < int > > g;
-bitset < MAX_N > visited;
+bitset < MAX_N > visited, used;
 int n,k,matching[MAX_K];
 // n = numero de nodos en izquierda
 // k = numero de nodos en la derecha
@@ -18,9 +18,21 @@ bool kuhn(int u) {
   return false;
 }
 int kuhn() {
-  int max_matching = 0;
+  int max_matching = 0,v;
   memset(matching, -1, sizeof matching);
-  foi(i,0,n) {
+  used.reset();
+  foi(u,0,n) // improvement
+    foi(i,0,g[u].size()){
+      v = g[u][i];
+      if(matching[v] == -1) {
+        ++max_matching;
+        matching[v] = u;
+        used[u] = true;
+        break;
+      }
+    }
+  foi(i,0,n) { // actual kuhn's algo
+    if(used[i]) continue;
     visited.reset();
     max_matching += kuhn(i);
   }
