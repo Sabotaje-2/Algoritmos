3260327de8d5871dc311efac9137bc3de38c3b55
Ivan Dario Sandoval <nola2804@hotmail.com>
2017-08-04 10:36:37 -0500

    Update Matrix exponentiation.cpp

--- Matematicas/Matrix exponentiation.cpp
+++ Matematicas/Matrix exponentiation.cpp
@@ -1,17 +1,17 @@
-vector<vi> mul(vector<vi>& a, vector<vi>& b) {
+vector<vi> mul(vector<vi>& a, vector<vi>& b, int MOD) {
     int n = a.size(), m = b[0].size();
     vector<vi> ans(n, vi(m, 0));
     foi(i,0,n)foi(j,0,m)foi(k,0,b.size())
-        ans[i][j] += (a[i][k] * b[k][j]);
+        ans[i][j] = (ans[i][j] + ((1LL * a[i][k] * b[k][j]) % MOD)) % MOD;
     return ans;
-}
-vector<vi> mpow(vector<vi>& mat, int exp) {
+} 
+vector<vi> mpow(vector<vi>& mat, int exp, int MOD) {
     vector<vi> ans(mat.size(), vi(mat.size(), 0)), power = mat;
     foi(i,0,ans.size()) ans[i][i] = 1;
     while(exp) {
         if(exp & 1)
-            ans = mul(ans, power);
-        power = mul(power, power);
+            ans = mul(ans, power, MOD);
+        power = mul(power, power, MOD);
         exp >>= 1;
     }
     return ans;
