af228e7c248912894a378b88a06f2c5e4922d4c3
Johan Sebastian Murillo Castillo <johan_smc@hotmail.com>
2018-03-27 00:48:02 -0500

    Create EuclideanExtended.cpp

---
+++ Matematicas/EuclideanExtended.cpp
@@ -0,0 +1,14 @@
+// if gcdex(a,b) == 1 no existe inverso modular
+int gcdex(int a, int b , int &x, int &y)
+{
+  if( a== 0 )
+  {
+    x = 0; y = 1 ;
+    return b;
+  }
+  int x1, y1;
+  int d = gcdex(b%a,a,x1,y1);
+  x = y1 - (b/a) * x1;
+  y = x1;
+  return d;
+}
