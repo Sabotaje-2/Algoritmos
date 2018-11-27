6c3ebc7afd8df8c05a76e06667e62857ff38712b
Iván Darío Sandoval <sandoval.i@javeriana.edu.co>
2018-11-27 00:30:36 -0500

    add max queue

---
+++ Estructuras de datos/Maximum Queue.cpp
@@ -0,0 +1,30 @@
+template<class T>
+struct max_stack {
+    private:
+    const T MINF = numeric_limits<T>::min(); // MENOS INFINITO
+    stack<pair<T,T>> s; // Element, Max
+    public:
+    void push(const T& x) {s.push(pair<T,T>(x,max(x,getmax())));}
+    void pop() {s.pop();}
+    bool empty() {return s.empty();}
+    T getmax() {return empty() ? MINF : s.top().second;}
+    T top() {return s.top().first;}};
+
+template<class T>
+struct max_queue {
+    private:
+    max_stack<T> s1, s2;
+    void transfer() {
+        if(s1.empty()) {
+            while(!s2.empty()) {
+                s1.push(s2.top());
+                s2.pop();
+            }
+        }
+    }
+    public:
+    void push(const T& x) {s2.push(x);}
+    void pop() {transfer();s1.pop();}
+    bool empty() {return s1.empty() && s2.empty();}
+    T getmax() {return max(s1.getmax(), s2.getmax());}
+    T front() {transfer(); return s1.top();}};
