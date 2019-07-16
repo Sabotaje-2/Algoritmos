59e0e95a033fff7379f48925f68f64e3f438b544
Iván Darío Sandoval <sandoval.i@javeriana.edu.co>
2019-07-16 05:07:17 -0500

    Update and rename Disjoint-set.cpp to DSU.cpp

---
+++ Estructuras de datos/DSU.cpp
@@ -0,0 +1,16 @@
+namespace DSU {
+    const int MAXN = 500*500;
+    int f[MAXN], sz[MAXN];
+    void build(int n) {
+        fill(sz, sz + n, 1);
+        foi(i,0,n) f[i] = i;
+    }
+    int find(int x) {return f[x] == x ? x : f[x] = find(f[x]);}
+    void join(int x, int y) {
+        int p = find(x), q = find(y);
+        if(p == q)  return;
+        if(sz[q] > sz[p]) swap(p, q);
+        sz[p] += sz[q];
+        f[q] = p;
+  }
+  bool connected(int i, int j) {return find(i) == find(j);}}
