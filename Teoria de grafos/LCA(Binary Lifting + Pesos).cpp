2039cb7aafd468185ec28e7ac41d684e4986ff40
Iván Darío Sandoval <sandoval.i@javeriana.edu.co>
2019-07-16 05:26:32 -0500

    Update LCA(Binary Lifting + Pesos).cpp

--- Teoria de grafos/LCA(Binary Lifting + Pesos).cpp
+++ Teoria de grafos/LCA(Binary Lifting + Pesos).cpp
@@ -1,42 +1,42 @@
 namespace lca {
     // build O(Vlog(V)), query O(log(V))
-    using Tedge = int; // Costo de las aristas
+    using T = int; // Costo de las aristas
     const int MAXN = 1+1000*1000; // Max No. Nodos
-    const int MAXLG = 21; // 2^(MAXLG-1) > MAXN
-    vector<pair<Tedge,int>> t[MAXN]; // u->{costo,v}
-    int f[MAXLG][MAXN], lv[MAXN];
-    Tedge pw[MAXLG][MAXN];
-    void dfs(int u, int lvl) {
-        lv[u] = lvl;
+    const int MLG = 21; // 2^(MLG-1) > MAXN
+    vector<pair<T,int>> t[MAXN]; // u->{costo,v}
+    int f[MLG][MAXN], l[MAXN];
+    T pw[MLG][MAXN];
+    void dfs(int u, int lu) {
+        l[u] = lu;
         for(auto p : t[u]) {
-            int v; Tedge c; tie(c,v) = p;
+            int v; T c; tie(c,v) = p;
             if(f[0][u] != v) {
                 f[0][v] = u, pw[0][v] = c;
-                dfs(v, 1 + lvl);
+                dfs(v, 1 + lu);
             }
         }
     }
     void build(int n, int r) { // |V|, nodo raiz
-        f[0][r] = r; pw[0][r] = numeric_limits<Tedge>::min();
+        f[0][r] = r; pw[0][r] = numeric_limits<T>::min();
         dfs(r,0);
-        foi(i,1,MAXLG) foi(u,0,n)
+        foi(i,1,MLG) foi(u,0,n)
             f[i][u] = f[i - 1][f[i - 1][u]],
             pw[i][u] = max(pw[i - 1][u], pw[i - 1][f[i - 1][u]]);
     }
-    pair<int,Tedge> query(int u, int v) { // {lca(u,v),max_edge(u,v)}
+    pair<int,T> query(int u, int v) { // {lca(u,v),max_edge(u,v)}
         if(u == v) assert(false); // definir el max_edge de u a u
-        if(lv[u] < lv[v]) swap(u,v);
-        Tedge medge = numeric_limits<Tedge>::min();
-        for(int i = MAXLG - 1; i >= 0; --i)
-            if(lv[f[i][u]] >= lv[v]) {
-                medge = max(medge, pw[i][u]);
+        if(l[u] < l[v]) swap(u,v);
+        T me = numeric_limits<T>::min();
+        for(int i = MLG - 1; i >= 0; --i)
+            if(l[f[i][u]] >= l[v]) {
+                me = max(me, pw[i][u]);
                 u = f[i][u];
             }
-        if(u == v) return {u,medge};
-        for(int i = MAXLG - 1; i >= 0; --i)
+        if(u == v) return {u,me};
+        for(int i = MLG - 1; i >= 0; --i)
             if(f[i][u] != f[i][v]) {
-                medge = max({medge, pw[i][u], pw[i][v]});
+                me = max({me, pw[i][u], pw[i][v]});
                 u = f[i][u], v = f[i][v];
             }
-        return {f[0][u], max({medge, pw[0][u], pw[0][v]})};
+        return {f[0][u], max({me, pw[0][u], pw[0][v]})};
     }}
