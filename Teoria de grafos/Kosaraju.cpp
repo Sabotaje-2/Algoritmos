43688681908a8daf0a9b13a831824c8f9a871a35
Iván Darío Sandoval <sandoval.i@javeriana.edu.co>
2019-07-26 22:57:44 -0500

    Update Kosaraju.cpp

--- Teoria de grafos/Kosaraju.cpp
+++ Teoria de grafos/Kosaraju.cpp
@@ -1,37 +1,27 @@
-struct kosaraju {
-    vector<vector<int>> revg, scc; // scc to nodes
-    vector<int> my_scc; // node to scc
-    stack<int> topo;
-    vector<bool> visited;
-    kosaraju(vector<vector<int>>& g) {
-        my_scc.resize(g.size());
-        visited.assign(g.size(), false);
-        revg.assign(g.size(), vector<int>());
-        foi(u,0,g.size()) {
-            if(!visited[u]) dfs(u,g);
-            for(const auto& v: g[u])
-                revg[v].push_back(u);
-        }
-        visited.assign(g.size(), false);
-        scc.reserve(g.size());
-        while(topo.size()) {
-            int u = topo.top();topo.pop();
-            if(!visited[u]) {
-                scc.push_back(vector<int>());
-                dfs2(u, revg);
-            }
-        }
+namespace kos {
+    const int MAXN = 1 + 100000;
+    vector<int> g[MAXN], rg[MAXN], scc[MAXN]; // scc to nodes
+    stack<int> ts;
+    int my_scc[MAXN], sccno; // node to scc, sccno = # of scc
+    bool vis[MAXN];
+    void dfs(int u, bool revg) {
+        vis[u] = true;
+        if(revg)
+            scc[sccno].push_back(u), my_scc[u] = sccno;
+        for(int v: (revg ? rg : g)[u])
+            if(!vis[v]) dfs(v,revg);
+        if(!revg) ts.push(u);
     }
-    void dfs(int u, vector<vector<int>>& g) {
-        visited[u] = true;
-        for(const auto& v: g[u])
-            if(!visited[v]) dfs(v,g);
-        topo.push(u);
-    }
-    void dfs2(int u, vector<vector<int>>& g) {
-        visited[u] = true;
-        scc[scc.size() - 1].push_back(u);
-        my_scc[u] = scc.size() - 1;
-        for(const auto& v: g[u])
-            if(!visited[v]) dfs2(v,g);
-    }};
+    void build(int n) {
+        fill(vis, vis + n, false);
+        foi(u,0,n) {
+            if(!vis[u]) dfs(u,false);
+            for(int v: g[u])
+                rg[v].push_back(u);
+        }
+        fill(vis, vis + n, false); sccno = 0;
+        while(!ts.empty()) {
+            int u = ts.top(); ts.pop();
+            if(!vis[u]) dfs(u,true), sccno++;
+        }
+    }}
