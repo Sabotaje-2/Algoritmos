1d16cfff1e6fecb3eed4a5575c588f5b31ed9780
Ivan Sandoval <sandoval.i@javeriana.edu.co>
2019-12-12 19:16:45 -0500

    Update Segment Tree.cpp

--- Estructuras de datos/Segment Tree.cpp
+++ Estructuras de datos/Segment Tree.cpp
@@ -1,29 +1,66 @@
-#define left ((pos << 1) | 1)
-#define right (1 + left)
-#define mid ((low + high) >> 1)
-struct stree {
-    vector<int> tree;
-    function<int(int,int)> op;
-    int sz;
-    stree(int N, function<int(int,int)> f) : op(f) {
-        assert(op); tree.resize((sz = N) << 2);
+#define le ((pos << 1) | 1)
+#define ri (1 + le)
+#define m ((s + e) >> 1)
+template<typename T>
+struct STree {
+  vector<T> tree;
+  function<T(T, T)> op;
+  int sz;
+  STree(int N, function<T(T,T)> f) : op(f) {
+    assert(op); tree.resize((sz = N) << 2);
+  }
+  void build(int arr[], int N) {
+    sz = N;
+    build(arr, 0, sz - 1, 0);
+  }
+  void build(int arr[], int s, int e, int pos) {
+    if (s == e) {
+      tree[pos] = arr[s];
+      return;
+    } else {
+      build(arr, s, m, le);
+      build(arr, 1 + m, e, ri);
+      tree[pos] = op(tree[le], tree[ri]);
     }
-    bool overlap(int qlow, int qhigh, int i, int j) {return !(qhigh < i || qlow > j);}
-    int build(int low, int high, int pos, vector<int>& arr) {
-        return tree[pos] = (low == high ? arr[low] : op(build(low, mid, left, arr), build(1 + mid, high, right, arr)));
+  }
+  bool overlap(int qs, int qe, int s, int e) {return !(qe < s || qs > e);}
+  T query(int& qs, int& qe, int s, int e, int pos) {
+    if(s >= qs && e <= qe)  return tree[pos];
+    if(overlap(qs, qe, s, m))
+      return overlap(qs, qe, 1 + m, e) ? op(query(qs, qe, s, m, le),
+          query(qs, qe, 1 + m, e, ri)) : query(qs, qe, s, m, le);
+    return query(qs, qe, 1 + m, e, ri);
+  }
+  void update(int s, int e, int pos, int x, T val) {
+    if(x >= s && x <= e) {
+      if(s == e) {
+        tree[pos] = val;
+      } else {
+        update(s, m, le, x, val), update(1 + m, e, ri, x, val);
+        tree[pos] = op(tree[le], tree[ri]);
+      }
     }
-    int query(int& qlow, int& qhigh, int low, int high, int pos) {
-        if(low >= qlow && high <= qhigh)  return tree[pos];
-        if(overlap(qlow, qhigh, low, mid))
-                return overlap(qlow, qhigh, 1 + mid, high) ? op(query(qlow, qhigh, low, mid, left), query(qlow, qhigh, 1 + mid, high, right)) : query(qlow, qhigh, low, mid, left);
-        return query(qlow, qhigh, 1 + mid, high, right);
-    }
-    int query(int i, int j) {return query(i, j, 0, sz - 1, 0);}
-    int update(int low, int high, int pos, int& x, int& val) {
-        if(x >= low && x <= high) {
-            if(low == high) return tree[pos] = val;
-            return tree[pos] = op(update(low, mid, left, x, val), update(1 + mid, high, right, x, val));
-        }
-        return tree[pos];
-    }
-    void update(int pos, int val) {update(0, sz - 1, 0, pos, val);}};
+  }
+  T query(int i, int j) {return query(i, j, 0, sz - 1, 0);} // check i <= j?
+  void update(int pos, T val) {update(0, sz - 1, 0, pos, val);}};
+
+struct SegTreeNode {
+  int min1, min2;
+  const SegTreeNode& operator=(int x) {
+    min1 = x;
+    min2 = INF;
+    return *this;
+  }
+};
+
+vector<STree<SegTreeNode>> st(10, STree<SegTreeNode>(MAXN, [](const SegTreeNode& p, const SegTreeNode& q)->SegTreeNode{
+  SegTreeNode s;
+  if (p.min1 <= q.min1) {
+    s.min1 = p.min1;
+    s.min2 = min(p.min2, q.min1);
+  } else {
+    s.min1 = q.min1;
+    s.min2 = min(q.min2, p.min1);
+  }
+  return s;
+}));
