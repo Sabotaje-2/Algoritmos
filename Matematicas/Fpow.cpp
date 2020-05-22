66ff255bd57133b6c2befa7c492c19756d99c80b
Ivan Sandoval <sandoval.i@javeriana.edu.co>
2020-05-21 20:24:11 -0500

    Update Fpow.cpp

--- Matematicas/Fpow.cpp
+++ Matematicas/Fpow.cpp
@@ -1,9 +1,9 @@
 // UVa 374.
 int fpow(int b, int e) {
-  int ans = 1, p = b;
+  int ans = 1;
   for (; e; e >>= 1) {
-    if(e & 1) ans = mul(ans, p);
-    p = mul(p, p);
+    if (e&1) ans = mul(ans, b);
+    b = mul(b, b);
   }
   return ans;
 }
