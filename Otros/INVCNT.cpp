d515a96be275f2f9c7c0c0d500476fd8394487d4
Ivan Sandoval <sandoval.i2804@gmail.com>
2020-09-04 15:01:00 -0500

    .

--- Otros/INVCNT.cpp
+++
@@ -1,9 +0,0 @@
-// SPOJ INVCNT.
-ll inversions(int arr[], int n) {
-  ll ans = 0, t = 0; ordered_set s;
-  for (int i = n - 1; i >= 0; --i) {
-    ans += (1LL * s.order_of_key(ii(arr[i], 0)));
-    s.insert(ii(arr[i], t++));
-  }
-  return ans;
-}
