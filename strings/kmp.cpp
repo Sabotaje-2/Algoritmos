5aa6a0e797f1db122241b0924c3a5ce36c529b93
Ivan Dario Sandoval Acosta <sandoval.i@javeriana.edu.co>
2024-09-08 00:13:48 -0500

    Refactor KMP

--- strings/kmp.cpp
+++ strings/kmp.cpp
@@ -2,8 +2,8 @@
 vector<int> pf(const string& s) {
   vector<int> pi(s.size(), 0);
   for (int i = 1, j = 0; i < (int)s.size(); ++i) {
-    while (j > 0 && s[i] != s[j]) j = pi[j-1];
-    if (s[i] == s[j]) ++j;
+    for (; j > 0 && s[i] != s[j]; j = pi[j-1]);
+    j += (s[i] == s[j]);
     pi[i] = j;
   }
   return pi;
