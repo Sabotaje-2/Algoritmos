cfd2a0e83cef8ca1a72fbcdbad5ab7e021f56877
Ivan Dario Sandoval Acosta <sandoval.i@javeriana.edu.co>
2024-09-07 23:34:20 -0500

    refactor suffix array

--- strings/suffix.cpp
+++ strings/suffix.cpp
@@ -1,22 +1,21 @@
 // Codeforces GYM 102028H.
 namespace sa {
-struct sfx {int idx, pos, posadd;};
-sfx v[MAXN];
-int arr[MAXLOG][MAXN], lk = 0;
+struct sfx {int idx, pos, posadd;} v[MAXN];
+int f[MAXLOG][MAXN], lk = 0;
 void build(const vector<int>& a, int n) {
-  for (int i = 0; i < n; ++i) arr[0][i] = a[i];
+  for (int i = 0; i < n; ++i) f[0][i] = a[i];
   for (int k = 1; (1 << (k-1)) <= n; lk = k++) {
     for (int i = 0; i < n; ++i) {
       int add = i + (1 << (k-1));
-      v[i] = sfx{i, arr[k-1][i], add < n ? arr[k-1][add] : -1};
+      v[i] = sfx{i, f[k-1][i], add < n ? f[k-1][add] : -1};
     }
     sort(v, v+n, [](const sfx& p, const sfx& q)->bool{
       return p.pos == q.pos ? p.posadd < q.posadd : p.pos < q.pos;
     });
     for (int i = 0; i < n; ++i) {
-      arr[k][v[i].idx] = i;
+      f[k][v[i].idx] = i;
       if (i > 0 && v[i].pos == v[i-1].pos && v[i].posadd == v[i-1].posadd)
-        arr[k][v[i].idx] = arr[k][v[i-1].idx];
+        f[k][v[i].idx] = f[k][v[i-1].idx];
     }
   }
 }};
